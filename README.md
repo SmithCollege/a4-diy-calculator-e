136498833d5a619eca0da1e3b7f645aa57fb3332
ezdancewic <ezdancewic@smith.edu>
2025-03-13 14:46:44 -0400

    assignment finished

--- README.md
+++ README.md
@@ -4,13 +4,21 @@
 
 Your readme should include the following information. Each student needs to submit all of this information themselves, even when pair programming. 
 
-Group Member Name(s):
+Group Member Name(s): Ellie Zdancewic
 
 Other collaborators you worked with, including TAs (and feel free to give a shoutout to anyone who was particularly helpful):
+TA's: Elaine & Jade Lilian, and Lan, Zhirou & Zihan. 
 
 Any references used besides JavaDoc and course materials:
+https://www.baeldung.com/java-instanceof
+https://www.geeksforgeeks.org/java-util-hashmap-in-java-with-examples/ 
+https://www.calcont.in/Conversion/infix_to_postfix  
 
 Reflection on your experience with this assignment:
+I feel like in retrospective these assignments always feel easier than they actually were, but there definitely were some frustrating moments. It was annoying that my tests wouldn't run individually and I could only verify them by building gradle. My general process for completing this assignment was to make a first "draft" of the algorithm by basically translating the words of the instructions into code. Once I had that up and running I did an extensive debugging process, first by fixing all of the errors to allow the code to run, and then using specific examples from the test to correct individual edge cases. It felt very much like whack-a-mole at some point and I ended up having pretty elaborate print statements to try and understand what in the world was going wrong. The most frustrating bug was figuring out that .remove removes the /first/ instance of the given object in a deque, not the one explicitly stated! I feel like deque, while it seems like its a good thing for it to function as both a stack and a queue, is actually worse because you don't know which very similar methods (ex. pop and remove) correspond to which functionality. I feel like it would've been easier to have two different data structures for stack and queues. 
+
+
 
 This assignment does lend itself to more visual prototyping, so please feel free to go this route. Are you including slides, pseudocode, comments, or something else?
 
+I am including comprehensive comments. I did take some notes by hand in order to debug, but I mostly used them to replicate the process of moving through the stacks and queues by erasing and rewriting so the final product isn't actually very informative.  
--- app/src/main/java/a4/Infix.java
+++ app/src/main/java/a4/Infix.java
@@ -1,11 +1,103 @@
 package a4;
 
 import java.util.ArrayDeque;
+import java.util.HashMap;
 
+/**
+ * A class that converts an infix expression into a postfix expression and evaluates it
+ */
 public class Infix {
 
-    public static Double infixToPostfix(ArrayDeque<Object> tokens) {
-        
+
+    /**
+     * A method converting an infix expression to postfix and evaluating it 
+     * @param tokens an ArrayDeque of Character and Double objects representing the infix expression 
+     * @return the resulting Double
+     */
+    public static double infixToPostfix(ArrayDeque<Object> tokens) {
+        ArrayDeque<Object> stack = new ArrayDeque<Object>();
+        ArrayDeque<Object> outputQueue = new ArrayDeque<Object>();
+        HashMap<Character, Integer> precedence = new HashMap<>();
+        precedence.put((Character)'(', 4);
+        precedence.put((Character)')', 4);
+        precedence.put((Character)'^', 3); 
+        precedence.put((Character)'*', 2);
+        precedence.put((Character)'/', 2);
+        precedence.put((Character)'+', 1);
+        precedence.put((Character)'-', 1); 
+
+        int tokensLength = tokens.size();
+        // read a token
+        for(int i = 0; i < tokensLength; i++){
+            System.out.println("");
+            System.out.println("tokens: " + tokens);
+            System.out.println("Stack: " + stack);
+            System.out.println("outputQueue: " + outputQueue);
+            System.out.println("");
+            // if the token is a number, add it to the output queue
+            if(tokens.getFirst() instanceof Double){
+                outputQueue.add(tokens.getFirst());
+                tokens.removeFirst();
+            } 
+            //if the token is an operator
+            else if(tokens.getFirst() instanceof Character && tokens.getFirst() != (Object)'(' && tokens.getFirst() != (Object)')'){
+                //while there is an operator token at the top of the stack and the stack operator has >= precedence
+                Object queueOperator = tokens.getFirst();
+                System.out.println("Stack: " + stack);
+                while(stack.size() > 0 && stack.getLast() instanceof Character && precedence.get(stack.getLast()) >= precedence.get(queueOperator) && stack.getLast() != (Object)'(' && stack.getLast() != (Object)')'){
+                    //pop the stack operator off the stack and add it to the output queue
+                    outputQueue.addLast(stack.getLast());
+                    stack.remove(stack.getLast());
+                }  
+                //when no more >= operators remain, push the queue operator onto the stack
+                stack.addLast(tokens.getFirst());
+                tokens.remove(tokens.getFirst());
+            }
+            //if the token is a left parenthesis, then push it onto the stack 
+            else if(tokens.getFirst() == (Object)'('){
+                stack.addLast(tokens.getFirst());
+                tokens.remove(tokens.getFirst());
+            } 
+            //if the token is a right parenthesis, 
+            else if(tokens.getFirst() == (Object)')'){
+                System.out.println("Right Paren Detected");
+                //until the token at the top of the stack is a left parenthesis
+                while(stack.size() > 0 && stack.getLast() != (Object)'('){
+                    //pop operators off the stack onto the output queue
+                    System.out.println("Stack: " + stack);
+                    System.out.println("Queue: " + outputQueue);
+                    System.out.println("stack.getLast() is " + stack.getLast() + " removing top stack operator");
+                    outputQueue.add(stack.getLast());
+                    stack.removeLast();
+                    System.out.println(stack.getLast());
+                }
+                //pop the left parenthesis from the stack, but not onto the output queue 
+                if(stack.size() > 0 && stack.getLast() == (Object)'('){
+                    stack.removeLast();
+                } else {
+                    //if the stack runs out without finding a left parenthesis,
+                    //then there are mismatched parentheses 
+                    throw new IllegalArgumentException("Mismatched parentheses");
+                }
+                tokens.removeFirst();
+            }
+        }
+        //when there are no more tokens to read
+        //while there are still tokens in the stack  
+        while(stack.size() > 0){
+            //if the token on top of the stack is a parenthesis, 
+            //then there are mismatched parentheses 
+            if(precedence.get(stack.getLast()) == 4){
+                throw new IllegalArgumentException("Mismatched parentheses");
+            } 
+            //if its an operator pop it onto the output queue 
+            else if(stack.getLast() instanceof Character){
+                outputQueue.add(stack.getLast());
+                stack.remove(stack.getLast());
+            }
+        }
+        //exit 
+        return Postfix.postfix(outputQueue);
     }
 
 }
